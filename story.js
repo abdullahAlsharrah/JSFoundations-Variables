144732dbee2118cbf99dafda966872b059583dc9
Abdullah Alsharrah <abdullahalsharrah@Abdullahs-Air.hotspot>
2020-10-18 19:42:21 +0300

    console.log task

---
+++ story.js
@@ -0,0 +1,6 @@
+let name = "Mshary";
+let age = 25;
+let hoppy = "Meditation";
+
+
+console.log(`Mr./Mrs. ${name}, in 33 years, you will be ${age + 33} years old. By then you will have decades of ${hoppy} practice!`);
